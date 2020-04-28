5f480d94d4143850fe7da543755a29ca76f2b9bb
aarooh <aaro.hassinen@gmail.com>
2020-04-28 18:33:16 +0300

    Commit
    Lazy to describe, sorry

--- plots.py
+++ plots.py
@@ -1,4 +1,4 @@
-# %%Main code structure
+# %%
 import numpy as np
 import pandas as pd
 import seaborn as sns 
@@ -10,9 +10,31 @@ data.describe(include='all')
 data.info()
 
 # Are values are non-null
+
 #Add categorical values to list
 categorical = ['job', 'marital', 'education', 'default', 'housing', 'loan', 'contact', 'month', 'poutcome']
+
 # Add numerical values to list
 numerical = [x for x in data.columns.to_list() if x not in categorical]
+
 # Remove last category the result from the numerical category
 numerical.remove('y')
+
+print('Categorical features:', categorical)
+print('Numerical features:', numerical)
+
+#Plot Answer variables of Answer attribute
+sns.countplot(x=data['y'])
+plt.title('Distribution of classes')
+plt.xlabel('Target class')
+
+# %%
+#Plot Pdays versus Answer Attribute
+sns.boxplot(y=data['pdays'], x=data['y'])
+plt.title('Box plot of pdays vs y (target variable)')
+plt.xlabel('y: target variable')
+
+
+
+
+# %%
