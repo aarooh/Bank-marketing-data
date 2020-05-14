122fe06709f830bfd7d4e6a3e1deff2c78aab628
aarooh <aaro.hassinen@gmail.com>
2020-05-14 19:32:59 +0300

    update outliers2

--- outliers.py
+++ outliers.py
@@ -6,22 +6,15 @@ from matplotlib import pyplot as plt
 from sklearn import preprocessing
 
 data = pd.read_csv('bank-full.csv', sep=';')
-data_numeric = data
-data.head()
+
 #sns.boxplot(x='y', y='duration', data=data)
+#sns.boxplot(x='y', y='balance', data=data)
+#sns.boxplot(x='y', y='age', data=data)
+#sns.boxplot(x='y', y='day', data=data)
+sns.boxplot(x='y', y='pdays', data=data)
 
-le = preprocessing.LabelEncoder()
 
-def categorize(data):
-    new_data = data.copy()
-    le = preprocessing.LabelEncoder()
-    new_data["job"] = le.fit_transform(new_data["job"])
-    new_data["education"] = le.fit_transform(new_data["education"])
-    return new_data
-
-new_data = categorize(data)
-new_data.head()
-# %%
-
+#sns.distplot(data["balance"])
+#sns.distplot(data["age"])
 
 # %%
